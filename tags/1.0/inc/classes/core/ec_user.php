31aa45eada67ea5c4b377ac7f30679471e393ccb
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-16 06:47:21 +0000

    Tagging the first version of WP EasyCart.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@741497 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0/inc/classes/core/ec_user.php
@@ -0,0 +1,143 @@
+<?php
+
+class ec_user{
+	protected $mysqli;									// ec_db structure
+	
+	public $user_id;									// INT
+	public $email;										// VARCHAR 255
+	public $user_level;									// VARCHAR 255
+	public $is_subscriber;								// BOOLEAN
+	
+	public $first_name;									// VARCHAR 255
+	public $last_name;									// VARCHAR 255
+	
+	public $billing_id;									// INT
+	public $shipping_id;								// INT
+	
+	public $billing;									// ec_address structure
+	public $shipping;									// ec_address structure
+	
+	private $password;									// VARCHAR 255
+	
+	function __construct( $email ){ 
+		
+		$this->mysqli = new ec_db();
+		
+		if( isset( $_SESSION['ec_user_id'] ) )
+			$this->user_id = $_SESSION['ec_user_id'];
+		else
+			$this->user_id = 0;
+		
+		if( isset( $_SESSION['ec_email'] ) )
+			$this->email = $_SESSION['ec_email'];
+		else
+			$this->email = "";
+			
+		if( isset( $_SESSION['ec_password'] ) )
+			$this->password = $_SESSION['ec_password'];
+		else
+			$this->password = "";
+		
+		$user = $this->mysqli->get_user( $this->email, $this->password );
+		
+		
+		
+		if( isset( $_SESSION['ec_first_name'] ) )
+			$this->first_name = $_SESSION['ec_first_name'];
+		else
+			$this->first_name = "";
+			
+		if( isset( $_SESSION['ec_last_name'] ) )
+			$this->last_name = $_SESSION['ec_last_name'];
+		else
+			$this->last_name = "";
+		
+		if( $user && $user->user_level ){
+			$this->user_level = $user->user_level;
+			$this->is_subscriber = $user->is_subscriber;
+			$this->billing_id = $user->default_billing_address_id;
+			$this->shipping_id = $user->default_shipping_address_id;
+		}else{
+			$this->user_level = "";
+			$this->is_subscriber = "";
+			$this->billing_id = "";
+			$this->shipping_id = "";
+		}
+		
+		if( isset( $_SESSION['ec_billing_first_name'] ) && $_SESSION['ec_billing_first_name'] != "" ){
+			$this->billing = new ec_address( $_SESSION['ec_billing_first_name'], $_SESSION['ec_billing_last_name'], $_SESSION['ec_billing_address'], "", $_SESSION['ec_billing_city'], $_SESSION['ec_billing_state'], $_SESSION['ec_billing_zip'], $_SESSION['ec_billing_country'], $_SESSION['ec_billing_phone'] );
+		}else if( $user && $user->billing_first_name ){
+			$this->billing = new ec_address( $user->billing_first_name, $user->billing_last_name, $user->billing_address_line_1, $user->billing_address_line_2, $user->billing_city, $user->billing_state, $user->billing_zip, $user->billing_country, $user->billing_phone );
+		}else{
+			$this->billing = new ec_address( "", "", "", "", "", "", "", "", "" );
+		}
+		
+		if( isset( $_SESSION['ec_shipping_first_name'] ) && $_SESSION['ec_shipping_first_name'] != "" ){
+			$this->shipping = new ec_address( $_SESSION['ec_shipping_first_name'], $_SESSION['ec_shipping_last_name'], $_SESSION['ec_shipping_address'], "", $_SESSION['ec_shipping_city'], $_SESSION['ec_shipping_state'], $_SESSION['ec_shipping_zip'], $_SESSION['ec_shipping_country'], $_SESSION['ec_shipping_phone'] );
+		}else if( $user && $user->shipping_first_name ){
+			$this->shipping = new ec_address( $user->shipping_first_name, $user->shipping_last_name, $user->shipping_address_line_1, $user->shipping_address_line_2, $user->shipping_city, $user->shipping_state, $user->shipping_zip, $user->shipping_country, $user->shipping_phone );
+		}else{
+			$this->shipping = new ec_address( "", "", "", "", "", "", "", "", "" );
+		}
+		
+	}
+	
+	private function setup_billing_info(){
+		
+		if(	isset($_POST['EmailNew']))		setup_billing_info_from_post();
+		else								setup_billing_info_from_db();
+	
+	}
+	
+	private function setup_shipping_info(){
+		
+		if(	isset($_POST['EmailNew']))		setup_shipping_info_from_post();
+		else								setup_shipping_info_from_db();
+	
+	}
+	
+	public function setup_billing_info_data( $bname, $blastname, $baddress, $bcity, $bstate, $bcountry, $bzip, $bphone ){
+		
+		$this->billing = new ec_address( $bname, $blastname, $baddress, "", $bcity, $bstate, $bcountry, $bzip, $bphone );
+		
+	}
+	
+	public function setup_shipping_info_data( $sname, $slastname, $saddress, $scity, $sstate, $scountry, $szip, $sphone ){
+		
+		$this->shipping = new ec_address( $sname, $slastname, $saddress, "", $scity, $sstate, $scountry, $szip, $sphone );
+		
+	}
+	
+	public function should_insert_user($userlevel, $createaccount){
+		if($userlevel == "guest" && $createaccount)					return true;
+		else 														return false;
+	}
+	
+	public function is_guest( ){
+		if( $_SESSION['ec_username'] == "guest" )					return true;
+		else														return false;	
+	}
+	
+	public function insert_user( ){
+		$this->billing_id = $this->insert_billing_info( );
+		$this->shipping_id = $this->insert_shipping_info( );
+		$this->user_id = $this->mysqli->insert_user( $this->email, $this->password, $this->first_name, $this->last_name, $this->billing_id, $this->shipping_id, "shopper", $this->is_subscriber );
+		$this->mysqli->update_address_user_id( $this->billing_id, $this->user_id );
+		$this->mysqli->update_address_user_id( $this->shipping_id, $this->user_id );
+	}
+	
+	public function insert_billing_info( ){
+		$this->mysqli->insert_address( $this->billing->first_name, $this->billing->last_name, $this->billing->address_line_1, $this->billing->city, $this->billing->state, $this->billing->zip, $this->billing->country, $this->billing->phone );
+	}
+	
+	public function insert_shipping_info( ){
+		$this->mysqli->insert_address( $this->shipping->first_name, $this->shipping->last_name, $this->shipping->address_line_1, $this->shipping->city, $this->shipping->state, $this->shipping->zip, $this->shipping->country, $this->shipping->phone );
+	}
+	
+	public function display_email( ){
+		echo $this->email;	
+	}
+	
+}
+
+?>
---
+++ tags/1.0/readme.txt
@@ -0,0 +1,86 @@
+=== WordPress Shopping Cart===
+Contributors: levelfourdevelopment
+Tags: Shopping Cart, eCommerce, e-Commerce, Storefront, Cart, EasyCart, WordPress Plugin, Wordpress shopping cart, Wordpress cart, Wordpress PayPal
+Requires at least: 3.0.1
+Tested up to: 3.5.2
+Stable tag: 1.0
+License: WP EasyCart License
+License URI: http://www.wpeasycart.com/terms-and-conditions/
+Donate Link: http://www.wpeasycart.com
+
+WP EasyCart lets you integrate eCommerce with a click of a button.
+
+== Description ==
+
+WP EasyCart eCommerce Plugin is a simple install into new or existing WordPress blogs. 
+Get a full eCommerce and shopping cart system in WordPress within minutes! Sell products, downloadable goods, gift cards, 
+clothing and more!  And now with WordPress, the powerful features are still very easy to administrate! 
+
+WP EasyCart provides administrative software, so there is no need to use WordPress as a slow management system for your products. Desktop PC, Mac, iPad, and Android software is available to download and allows you to control the myriad of options the software has to offer.
+
+== Installation ==
+
+1.  Create 3 new pages in WordPress: An example of the three page names would be 'Store', 'Shopping Cart', and 'My Account'.
+
+2.  Open each page you just created and enter the WordPress shortcode to apply the eCommerce system to those pages. A short code is a word in brackets that is replaced by a plugin or widget when displayed to the user. The image on the right is a screenshot of adding the store shortcode to the store page.
+
+     a.  Open the 'Store' page and type **[ec_store]** in the page text editor.
+	 
+	 b.  Open 'Shopping Cart' page and type **[ec_cart]** in the page text editor.
+	 
+     c.  Open 'My Account' page and type **[ec_account]** in the page text editor.
+
+3.  Select the pages you just created in the select boxes below. The will help the WP EasyCart link your store up correctly. That's it! Now you can configure your store or access the admin console area.
+
+**We have full documentation at [WP EasyCart](http://www.wpeasycart.com/ "WP EasyCart website") including an online users manual, video tutorials, and community forums.**
+
+== Frequently Asked Questions ==
+
+= Can I use your software to display, but not sell products? =
+
+Absolutely! If you do not need to sell anything, but would like to leverage our software to display products, please feel free to do just that!
+
+= How does a license work with your software? =
+
+The free plugin is the full software version, however, the administrative software that is downloaded from our website is licensed software.  The administrative software will allow a certain number of products and features to be used in the software.  You may continue to run th WordPress plugin, but must purchase a license to use our administrative software.
+
+Each license is good for one website url or wordpress installation.  You may transfer a license from a development environment to a production environment at anytime, it is your license once you purchase one.
+
+Licensing gives you features such as coupons, promotions, live shipping rate calcuators and more.  Plus you will receive support from our staff individually rather than through our community open support systems.  For more information on licensing, you can view our website.
+
+=  Do I have to pay monthly? =
+
+Not to us.  Our standard plugin is a one time purchase.  Your payment gateway provider such as PayPal, Authorize.net, FirstData or others may or may not have monthly fees, but nothing through EasyCart.
+
+If you purchase a monthly hosted plan, then we do have a monthly charge program for those who prefer this method.
+
+= Can I display specials on any page? =
+
+Yes you can! Simply go to the widgets page, and drag over the specials widget to any widget area. You can then check a box
+on the product you want to display in the specials area.
+
+= How do I add a vertical or horizontal store menu to my WordPress site? =
+
+Simply go to the widgets page, and drag over the store menu widget to any widget area.
+
+= How do I add categories, manufacturers, or price range filters to my WordPress site? =
+
+Simply go to the widgets page, and drag over the widget cooresponding to the filter you wish to display to any widget area.
+
+== Screenshots ==
+
+1. Select from several professionally built theme designs!
+2. Beautiful product option and image displays!
+3. Huge selection of features, coupons, promotions, and live shipping options!
+4. Complete administrative software for desktops and mobile devices!
+
+== Changelog ==
+
+= 1.0 =
+* First Release store version 1.0
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+Be sure to backup all files and database before completing an upgrade of any kind.  In the event an upgrade causes a conflict, you may revert back to this backup.
