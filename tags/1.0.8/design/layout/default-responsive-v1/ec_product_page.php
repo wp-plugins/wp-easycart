ee208a62f87f60d59b67c3d33711542ccd2eccb6
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-18 20:48:09 +0000

    Updates to admin console downloads and installation instructions.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@742916 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.8/design/layout/default-responsive-v1/ec_product_page.php
@@ -0,0 +1,10 @@
+<div class="ec_product_page" id="ec_product_page">
+	<?php if( $this->has_products( ) ){ ?>
+   	<div><?php $this->product_filter_bar(); ?></div>
+    <div><?php $this->product_list(); ?></div>
+    <div class="filter_bar_bottom"><?php $this->product_filter_bar(); ?></div>
+    <div style="clear:both"></div>
+    <?php }else{ ?>
+    <div class="ec_products_no_results"><?php echo $GLOBALS['language']->get_text( "product_page", "product_no_results" ); ?></div>
+    <?php }?>
+</div>
