919301dec33939227d417eb5a907c8127e7a3a79
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-22 20:34:10 +0000

    upgraded to 1.0.9
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@744593 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.9/design/layout/base-responsive-v1/ec_account_dashboard.php
@@ -0,0 +1,114 @@
+<div id="ec_account_dashboard">
+  <div class="ec_account_dashboard_holder_main">
+    <div class="ec_account_dashboard_left">
+      <div class="ec_account_dashboard_title"><?php echo $GLOBALS['language']->get_text( 'account_dashboard', 'account_dashboard_recent_orders_title' )?></div>
+      <div class="ec_account_dashboard_holder">
+        <?php if( $this->orders->num_orders > 0 ){ ?>
+        <div class="ec_account_dashboard_row" id="ec_dashboard_orders_list">
+          <div class="ec_account_order_line_header">
+            <div class="ec_account_order_line_column1_header"><?php echo $GLOBALS['language']->get_text( 'account_orders', 'account_orders_header_1' )?></div>
+            <div class="ec_account_order_line_column2_header"><?php echo $GLOBALS['language']->get_text( 'account_orders', 'account_orders_header_2' )?></div>
+            <div class="ec_account_order_line_column3_header"><?php echo $GLOBALS['language']->get_text( 'account_orders', 'account_orders_header_3' )?></div>
+            <div class="ec_account_order_line_column4_header"><?php echo $GLOBALS['language']->get_text( 'account_orders', 'account_orders_header_4' )?></div>
+            <div class="ec_account_order_line_column5_header"></div>
+          </div>
+          <?php $days_prior_to_show = 21; $max_orders_to_show = 10; $this->orders->display_order_list( $days_prior_to_show, $max_orders_to_show ); ?>
+        </div>
+        <div class="ec_account_dashboard_row_divider">
+          <?php $this->display_orders_link( $GLOBALS['language']->get_text( 'account_dashboard', 'account_dashboard_all_orders_linke' ) ); ?>
+        </div>
+        <?php }else{ echo $GLOBALS['language']->get_text( "account_dashboard", "account_dashboard_recent_orders_none" ); }?>
+      </div>
+      <div class="ec_account_dashboard_title"><?php echo $GLOBALS['language']->get_text( 'account_dashboard', 'account_dashboard_preference_title' )?></div>
+      <div class="ec_account_dashboard_holder">
+        <div class="ec_account_dashboard_row_bold"><?php echo $GLOBALS['language']->get_text( 'account_dashboard', 'account_dashboard_email_title' )?></div>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_email">
+          <?php $this->user->display_email(); ?>
+        </div>
+        <div class="ec_account_dashboard_row_content">
+          <?php $this->display_personal_information_link( $GLOBALS['language']->get_text( 'account_dashboard', 'account_dashboard_email_edit_link' ) ); ?>
+          <?php echo $GLOBALS['language']->get_text( 'account_dashboard', 'account_dashboard_email_note' )?></div>
+        <div class="ec_account_dashboard_row_bold"><b><?php echo $GLOBALS['language']->get_text( 'account_dashboard', 'account_dashboard_billing_title' )?></b></div>
+        <?php if( $this->user->billing->first_name || $this->user->billing->last_name ){ ?>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_billing_name">
+          <?php $this->user->billing->display_first_name(); ?>
+          <?php $this->user->billing->display_last_name(); ?>
+        </div>
+        <?php } ?>
+        <?php if( $this->user->billing->address_line_1 ){ ?>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_billing_address_line_1">
+          <?php $this->user->billing->display_address_line_1(); ?>
+        </div>
+        <?php } ?>
+        <?php if( $this->user->billing->city || $this->user->billing->state || $this->user->billing->zip ){ ?>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_billing_address_line_2">
+          <?php $this->user->billing->display_city(); ?>
+          ,
+          <?php $this->user->billing->display_state(); ?>
+          <?php $this->user->billing->display_zip(); ?>
+        </div>
+        <?php } ?>
+        <?php if( $this->user->billing->country ){ ?>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_billing_country">
+          <?php $this->user->billing->display_country(); ?>
+        </div>
+        <?php } ?>
+        <?php if( $this->user->billing->phone ){ ?>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_billing_phone">
+          <?php $this->user->billing->display_phone(); ?>
+        </div>
+        <?php } ?>
+        <div class="ec_account_dashboard_row_content">
+          <?php $this->display_billing_information_link( $GLOBALS['language']->get_text( 'account_dashboard', 'account_dashboard_billing_link' ) ); ?>
+        </div>
+        <div class="ec_account_dashboard_row_bold"><b><?php echo $GLOBALS['language']->get_text( 'account_dashboard', 'account_dashboard_shipping_title' )?></b></div>
+        <?php if( $this->user->shipping->first_name || $this->user->shipping->last_name ){ ?>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_shipping_name">
+          <?php $this->user->shipping->display_first_name(); ?>
+          <?php $this->user->shipping->display_last_name(); ?>
+        </div>
+        <?php } ?>
+        <?php if( $this->user->shipping->address_line_1 ){ ?>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_shipping_address_line_1">
+          <?php $this->user->shipping->display_address_line_1(); ?>
+        </div>
+        <?php } ?>
+        <?php if( $this->user->shipping->city || $this->user->shipping->state || $this->user->shipping->zip ){ ?>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_shipping_address_line_2">
+          <?php $this->user->shipping->display_city(); ?>
+          ,
+          <?php $this->user->shipping->display_state(); ?>
+          <?php $this->user->shipping->display_zip(); ?>
+        </div>
+        <?php } ?>
+        <?php if( $this->user->shipping->country ){ ?>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_shipping_country">
+          <?php $this->user->shipping->display_country(); ?>
+        </div>
+        <?php } ?>
+        <?php if( $this->user->shipping->phone ){ ?>
+        <div class="ec_account_dashboard_row_content" id="ec_account_dashboard_shipping_phone">
+          <?php $this->user->shipping->display_phone(); ?>
+        </div>
+        <?php } ?>
+        <div class="ec_account_dashboard_row_content">
+          <?php $this->display_shipping_information_link( $GLOBALS['language']->get_text( 'account_dashboard', 'account_dashboard_shipping_link' )); ?>
+        </div>
+      </div>
+    </div>
+    <div class="ec_account_dashboard_right">
+      <div class="ec_account_dashboard_title"><?php echo $GLOBALS['language']->get_text( 'account_navigation', 'account_navigation_title' )?></div>
+      <div class="ec_account_dashboard_holder">
+        <div class="ec_account_dashboard_row_content">
+          <?php $this->display_personal_information_link( $GLOBALS['language']->get_text( 'account_navigation', 'account_navigation_basic_inforamtion' ) ); ?>
+        </div>
+        <div class="ec_account_dashboard_row_content">
+          <?php $this->display_password_link( $GLOBALS['language']->get_text( 'account_navigation', 'account_navigation_password' ) ); ?>
+        </div>
+        <div class="ec_account_dashboard_row_content">
+          <?php $this->display_logout_link( $GLOBALS['language']->get_text( 'account_navigation', 'account_navigation_sign_out' )); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
