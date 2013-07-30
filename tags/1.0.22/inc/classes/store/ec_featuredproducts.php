29b7403bbec7fbc3d8c0523ae77e9d8732cf809b
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-30 22:20:41 +0000

    More bug fixes. Version 22 out.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@748515 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.22/inc/classes/store/ec_featuredproducts.php
@@ -0,0 +1,42 @@
+<?php
+
+class ec_featuredproducts{
+	protected $mysqli;									// ec_db structure
+	
+	public $product1;									// ec_product structure
+	public $product2;									// ec_product structure
+	public $product3;									// ec_product structure
+	public $product4;									// ec_product structure
+	
+	function __construct( $product_id_1, $product_id_2, $product_id_3, $product_id_4 ){
+		
+		if($product_id_1 || $product_id_2 || $product_id_3 || $product_id_4 ){
+			$this->mysqli = new ec_db( );
+														$where_statement  = " WHERE (";
+														$where_statement .= " product.product_id = '"  . $product_id_1 . "'";
+														$where_statement .= " OR product.product_id = '"  . $product_id_2 . "'";
+														$where_statement .= " OR product.product_id = '"  . $product_id_3 . "'";
+														$where_statement .= " OR product.product_id = '"  . $product_id_4 . "'";
+														$where_statement .= " ) ";
+											
+														$order_statement  = " ORDER BY FIELD(product.product_id ";
+			if( $product_id_1 )							$order_statement .= ", '" . $product_id_1 . "'";
+			if( $product_id_2 )							$order_statement .= ", '" . $product_id_2 . "'";
+			if( $product_id_3 )							$order_statement .= ", '" . $product_id_3 . "'";
+			if( $product_id_4 )							$order_statement .= ", '" . $product_id_4 . "'";
+														$order_statement .= ") ";
+				
+			
+			$products = $this->mysqli->get_product_list( $where_statement, $order_statement, "", session_id() );
+			
+			if( count( $products ) > 0 )				$this->product1 = new ec_product( $products[0], 1 );
+			if( count( $products ) > 1 )				$this->product2 = new ec_product( $products[1], 1 );
+			if( count( $products ) > 2 )				$this->product3 = new ec_product( $products[2], 1 );
+			if( count( $products ) > 3 )				$this->product4 = new ec_product( $products[3], 1 );
+		}
+		
+	}
+	
+}
+
+?>
