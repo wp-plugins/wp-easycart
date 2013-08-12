0dddfeae78fc8ed09559691d5dbdc2a37d51872d
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-12 04:53:06 +0000

    Tagging 1.1.1, check changelog for more info.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@754949 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.1.1/inc/amfphp/Amfphp/Services/dashboard.php
@@ -0,0 +1,130 @@
+<?php
+/*
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//All Code and Design is copyrighted by Level Four Development, llc
+//
+//Level Four Development, LLC provides this code "as is" without warranty of any kind, either express or implied,     
+//including but not limited to the implied warranties of merchantability and/or fitness for a particular purpose.         
+//
+//Only licnesed users may use this code and storfront for live purposes. All other use is prohibited and may be 
+//subject to copyright violation laws. If you have any questions regarding proper use of this code, please
+//contact Level Four Development, llc and EasyCart prior to use.
+//
+//All use of this storefront is subject to our terms of agreement found on Level Four Development, llc's  website.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+class dashboard
+	{		
+	
+		function dashboard() {
+			//load our connection settings
+			require_once('../../../connection/ec_conn.php');
+		
+			//set our connection variables
+			$dbhost = HOSTNAME;
+			$dbname = DATABASE;
+			$dbuser = USERNAME;
+			$dbpass = PASSWORD;	
+
+			//make a connection to our database
+			$this->conn = mysql_connect($dbhost, $dbuser, $dbpass);
+			mysql_select_db ($dbname);	
+		}	
+			
+		
+		//HELPER - used to escape out SQL calls
+		function escape($sql) 
+		{ 
+			  $args = func_get_args(); 
+				foreach($args as $key => $val) 
+				{ 
+					$args[$key] = mysql_real_escape_string($val); 
+				} 
+				 
+				$args[0] = $sql; 
+				return call_user_func_array('sprintf', $args); 
+		} 
+		
+		
+		//dashboard functions
+		function getdashboardorders() {
+			  //Create SQL Query
+			  $query= mysql_query("SELECT SQL_CALC_FOUND_ROWS ec_order.billing_first_name, ec_order.billing_last_name, ec_order.grand_total, UNIX_TIMESTAMP(ec_order.order_date) AS order_date, ec_order.user_email, ec_order.orderstatus_id, ec_order.order_id, ec_order.order_viewed FROM ec_order  ORDER BY ec_order.order_date DESC LIMIT 0, 23");
+			  
+			  $totalquery=mysql_query("SELECT FOUND_ROWS()");
+			  $totalrows = mysql_fetch_object($totalquery);
+			  
+			  //if results, convert to an array for use in  flash
+			  if(mysql_num_rows($query) > 0) {
+				  while ($row=mysql_fetch_object($query)) {
+					 $row->totalrows=$totalrows;
+					 $returnArray[] = $row;
+				  }
+				  return($returnArray); //return array results if there are some
+			  } else {
+				  $returnArray[] = "noresults";
+				  return $returnArray; //return noresults if there are no results
+			  }
+		}
+		
+		function getdashboardmenus() {
+			  //Create SQL Query
+			  $query= mysql_query("SELECT SQL_CALC_FOUND_ROWS ec_menulevel1.* FROM ec_menulevel1 ORDER BY ec_menulevel1.clicks DESC LIMIT 0, 6");
+			  $totalquery=mysql_query("SELECT FOUND_ROWS()");
+			  $totalrows = mysql_fetch_object($totalquery);
+			  
+			  //if results, convert to an array for use in flash
+			  if(mysql_num_rows($query) > 0) {
+				  while ($row=mysql_fetch_object($query)) {
+					  $row->totalrows=$totalrows;
+					  $returnArray[] = $row;
+				  }
+				  return($returnArray); //return array results if there are some
+			  } else {
+				  $returnArray[] = "noresults";
+				  return $returnArray; //return noresults if there are no results
+			  }
+		}
+		function getdashboardreviews() {
+			  //Create SQL Query
+			  $query= mysql_query("SELECT SQL_CALC_FOUND_ROWS  ec_product.title, reviews.*, UNIX_TIMESTAMP(reviews.datesubmitted) AS datesubmitted  FROM reviews  LEFT JOIN products ON ec_product.product_id = reviews.productID ORDER BY reviews.datesubmitted DESC LIMIT 0, 6");
+			  $totalquery=mysql_query("SELECT FOUND_ROWS()");
+			  $totalrows = mysql_fetch_object($totalquery);
+			  
+			  //if results, convert to an array for use in flash
+			  if(mysql_num_rows($query) > 0) {
+				  while ($row=mysql_fetch_object($query)) {
+					  $row->totalrows=$totalrows;
+					  $returnArray[] = $row;
+				  }
+				  return($returnArray); //return array results if there are some
+			  } else {
+				  $returnArray[] = "noresults";
+				  return $returnArray; //return noresults if there are no results
+			  }
+		}
+		function getdashboardproducts() {
+			  //Create SQL Query
+			  $query= mysql_query("SELECT SQL_CALC_FOUND_ROWS ec_product.*, statistics.* FROM products LEFT JOIN statistics ON ec_product.product_id = statistics.ProductID ORDER BY statistics.views DESC LIMIT 0, 6");
+			  $totalquery=mysql_query("SELECT FOUND_ROWS()");
+			  $totalrows = mysql_fetch_object($totalquery);
+			  
+			  //if results, convert to an array for use in flash
+			  if(mysql_num_rows($query) > 0) {
+				  while ($row=mysql_fetch_object($query)) {
+					  $row->totalrows=$totalrows;
+					  $returnArray[] = $row;
+				  }
+				  return($returnArray); //return array results if there are some
+			  } else {
+				  $returnArray[] = "noresults";
+				  return $returnArray; //return noresults if there are no results
+			  }
+		}
+
+	}//close class
+?>
---
+++ tags/1.1.1/inc/amfphp/Amfphp/Services/perpage.php
@@ -0,0 +1,125 @@
+<?php
+/*
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//All Code and Design is copyrighted by Level Four Development, llc
+//
+//Level Four Development, LLC provides this code "as is" without warranty of any kind, either express or implied,     
+//including but not limited to the implied warranties of merchantability and/or fitness for a particular purpose.         
+//
+//Only licnesed users may use this code and storfront for live purposes. All other use is prohibited and may be 
+//subject to copyright violation laws. If you have any questions regarding proper use of this code, please
+//contact Level Four Development, llc and EasyCart prior to use.
+//
+//All use of this storefront is subject to our terms of agreement found on Level Four Development, llc's  website.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+class perpage
+	{		
+	
+		function perpage() {
+			//load our connection settings
+			require_once('../../../connection/ec_conn.php');
+		
+			//set our connection variables
+			$dbhost = HOSTNAME;
+			$dbname = DATABASE;
+			$dbuser = USERNAME;
+			$dbpass = PASSWORD;	
+
+			//make a connection to our database
+			$this->conn = mysql_connect($dbhost, $dbuser, $dbpass);
+			mysql_select_db ($dbname);	
+
+		}	
+			
+		
+		//HELPER - used to escape out SQL calls
+		function escape($sql) 
+		{ 
+			  $args = func_get_args(); 
+				foreach($args as $key => $val) 
+				{ 
+					$args[$key] = mysql_real_escape_string($val); 
+				} 
+				 
+				$args[0] = $sql; 
+				return call_user_func_array('sprintf', $args); 
+		} 
+		
+		
+		
+		//per page functions
+		function getperpage() {
+			//Create SQL Query
+			$sql = "SELECT * FROM ec_perpage ORDER BY perpage ASC";
+			
+			$result = mysql_query($sql);
+			  //if results, convert to an array for use in flash
+			  if(mysql_num_rows($result) > 0) {
+				  while ($row=mysql_fetch_object($result)) {
+					  $returnArray[] = $row;
+				  }
+				  return($returnArray); //return array results if there are some
+			  } else {
+				  $returnArray[] = "noresults";
+				  return $returnArray; //return noresults if there are no results
+			  }
+		}
+		
+		function updateperpage($id, $perpage) {
+			//Create SQL Query
+			  $sql = $this->escape("UPDATE ec_perpage SET ec_perpage.perpage='%s' WHERE ec_perpage.perpage_id = '%s'", $perpage, $id);
+			//Run query on database;
+			mysql_query($sql);
+			//if no errors, return their current Client ID
+			//if results, convert to an array for use in flash
+			if(!mysql_error()) {
+				$returnArray[] ="success";
+				return($returnArray); //return array results if there are some
+			} else {
+				$returnArray[] = "error";
+				return $returnArray; //return noresults if there are no results
+			}
+		}	
+		function deleteperpage($id) {
+			//Create SQL Query
+			$sql = $this->escape("DELETE FROM ec_perpage WHERE ec_perpage.perpage_id = %s", $id);
+			//Run query on database;
+			mysql_query($sql);
+			//if no errors, return their current Client ID
+			//if results, convert to an array for use in flash
+			if(!mysql_error()) {
+				$returnArray[] ="success";
+				return($returnArray); //return array results if there are some
+			} else {
+				$returnArray[] = "error";
+				return $returnArray; //return noresults if there are no results
+			}
+		}	
+		function addperpage($perpage) {
+			//Create SQL Query
+			$sql = sprintf("Insert into ec_perpage(ec_perpage.perpage_id, ec_perpage.perpage)
+				values(null, '%s')",
+				mysql_real_escape_string($perpage));
+			//Run query on database;
+			mysql_query($sql);
+			//if no errors, return their current Client ID
+			//if results, convert to an array for use in flash
+			if(!mysql_error()) {
+				$returnArray[] ="success";
+				return($returnArray); //return array results if there are some
+			} else {
+				$returnArray[] = "error";
+				return $returnArray; //return noresults if there are no results
+			}
+		}
+		
+		//////END PER PAGE/////
+		
+
+	}//close class
+?>
