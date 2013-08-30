1dce95187442e43de6f928af191cc0241e4079a6
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-30 20:24:11 +0000

    Tagging 1.1.17, check change log for more info.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@764918 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.1.17/inc/amfphp/Amfphp/Services/database.php
@@ -0,0 +1,119 @@
+<?php
+/*
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//All Code and Design is copyrighted by Level Four Development, llc
+//
+//Level Four Development, LLC provides this code "as is" without warranty of any kind, either express or implied,     
+//including but not limited to the implied warranties of merchantability and/or fitness for a particular purpose.         
+//
+//Only licnesed users may use this code and storfront for live purposes. All other use is prohibited and may be 
+//subject to copyright violation laws. If you have any questions regarding proper use of this code, please
+//contact Level Four Development, llc and EasyCart prior to use.
+//
+//All use of this storefront is subject to our terms of agreement found on Level Four Development, llc's  website.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+class database
+	{		
+	
+		function database() {
+			//load our connection settings
+			require_once('../../../connection/ec_conn.php');
+		
+			//set our connection variables
+			$dbhost = HOSTNAME;
+			$dbname = DATABASE;
+			$dbuser = USERNAME;
+			$dbpass = PASSWORD;	
+
+			//make a connection to our database
+			$this->conn = mysql_connect($dbhost, $dbuser, $dbpass);
+			mysql_select_db ($dbname);	
+			mysql_query("SET CHARACTER SET utf8", $this->conn); 
+			mysql_query("SET NAMES 'utf8'", $this->conn); 
+
+		}	
+			
+		
+		//HELPER - used to escape out SQL calls
+		function escape($sql) 
+		{ 
+			  $args = func_get_args(); 
+				foreach($args as $key => $val) 
+				{ 
+					$args[$key] = mysql_real_escape_string($val); 
+				} 
+				 
+				$args[0] = $sql; 
+				return call_user_func_array('sprintf', $args); 
+		} 
+		
+		
+			
+		
+		//database  functions
+		function restoredb($dbfilename) {
+			// Get file data
+			$data = file("../../../products/".$dbfilename);
+			//echo $data;
+			// Temporary variable, used to store current query
+			$templine = '';
+			// Read in entire file
+			$lines = $data;
+			// Loop through each line
+			foreach ($lines as $line_num => $line) {
+			  // Only continue if it's not a comment
+			  if (substr($line, 0, 2) != '/*' && $line != '') {
+				// Add this line to the current segment
+				$templine .= $line;
+				
+				// If it has a semicolon at the end, it's the end of the query
+				if (substr(trim($line), -1, 1) == ';') {
+				   // Perform the query
+				    mysql_query($templine); 
+					 // Reset temp variable to empty
+				  $templine = '';
+				}
+			  }
+			}
+			//if results, convert to an array for use in flash
+			if(!mysql_error()) {
+				$returnArray[] ="success";
+				return($returnArray); //return array results if there are some
+			} else {
+				$returnArray[] = "error";
+				return $returnArray; //return noresults if there are no results
+			}
+					  
+		}
+		
+		function deletedbfile($dbfilename) {
+			if (unlink("../../../products/".$dbfilename)) {
+				return "success";
+			} else {
+				return "error";
+			}
+		}
+		
+		function logdbrestore() {
+			//insert into db backup log an entry
+			//Create SQL Query
+			$sql = sprintf("INSERT INTO ec_dblog(ec_dblog.entry_date, ec_dblog.entry_type) values(NOW(), 'restore')");
+			//Run query on database;
+			mysql_query($sql);
+		}
+		function logdbbackup() {
+			//insert into db backup log an entry
+			//Create SQL Query
+			$sql = sprintf("INSERT INTO ec_dblog(ec_dblog.entry_date, ec_dblog.entry_type) values(NOW(), 'backup')");
+			//Run query on database;
+			mysql_query($sql);
+		}
+
+
+	}//close class
+?>
---
+++ tags/1.1.17/inc/classes/widget/ec_searchwidget.php
@@ -0,0 +1,65 @@
+<?php 
+class ec_searchwidget extends WP_Widget{
+	
+	function ec_searchwidget(){
+		$widget_ops = array('classname' => 'ec_searchwidget', 'description' => 'Displays the Product Search Filter For Your WP EasyCart' );
+		$this->WP_Widget('ec_searchwidget', 'WP EasyCart Product Search Filter', $widget_ops);
+	}
+	
+	function form( $instance ){ 
+		if( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}else {
+			$title = __( 'Product Search', 'text_domain' );
+		}
+		
+		if( isset( $instance[ 'label' ] ) ) {
+			$label = $instance[ 'label' ];
+		}else {
+			$label = __( 'Search', 'text_domain' );
+		}
+		
+		echo "<p><label for=\"" . $this->get_field_name( 'title' ) . "\">" . _e( 'Title:' ) . "</label><input class=\"widefat\" id=\"" . $this->get_field_id( 'title' ) . "\" name=\"" . $this->get_field_name( 'title' ) . "\" type=\"text\" value=\"" . esc_attr( $title ) . "\" /></p>";
+		
+		echo "<p><label for=\"" . $this->get_field_name( 'label' ) . "\">" . _e( 'Title:' ) . "</label><input class=\"widefat\" id=\"" . $this->get_field_id( 'label' ) . "\" name=\"" . $this->get_field_name( 'label' ) . "\" type=\"text\" value=\"" . esc_attr( $label ) . "\" /></p>";
+		
+		$defaults = array();
+		$instance = wp_parse_args( (array) $instance, $defaults);
+	}
+	
+	function update($new_instance, $old_instance){
+		$instance = array();
+		$instance['title'] = ( !empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['label'] = ( !empty( $new_instance['label'] ) ) ? strip_tags( $new_instance['label'] ) : '';
+
+		return $instance;
+	}
+	
+	
+	function widget($args, $instance){
+	
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		$label = apply_filters( 'widget_label', $instance['label'] );
+	
+		echo $before_widget;
+		if ( ! empty( $title ) )
+			echo $before_title . $title . $after_title;
+			
+		// WIDGET CODE GOES HERE
+		$mysqli = new ec_db();
+		$filter = new ec_filter(0);
+		
+		$storepageid = get_option('ec_option_storepage');
+		$store_page = get_permalink( $storepageid );
+		
+		if( substr_count( $store_page, '?' ) )						$permalink_divider = "&";
+		else														$permalink_divider = "?";
+		
+		include("wp-content/plugins/wp-easycart/design/layout/" . get_option( 'ec_option_base_layout' ) . "/ec_search_widget.php");
+		
+		echo $after_widget;
+	}
+ 
+}
+?>
