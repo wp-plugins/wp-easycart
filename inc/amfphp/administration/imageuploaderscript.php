0349af8eae0744219bcc280f706b455a08fb049f
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-19 14:45:17 +0000

    added all admin access plus if wordpress user is logged in may access script.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart/trunk@775408 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/amfphp/administration/imageuploaderscript.php
+++ inc/amfphp/administration/imageuploaderscript.php
@@ -7,7 +7,7 @@
 //Level Four Development, LLC provides this code "as is" without warranty of any kind, either express or implied,     
 //including but not limited to the implied warranties of merchantability and/or fitness for a particular purpose.         
 //
-//Only licnesed users may use this code and storfront for live purposes. All other use is prohibited and may be 
+//Only licensed users may use this code and storfront for live purposes. All other use is prohibited and may be 
 //subject to copyright violation laws. If you have any questions regarding proper use of this code, please
 //contact Level Four Development, llc and EasyCart prior to use.
 //
@@ -81,7 +81,7 @@ $userresult = mysql_query($usersqlquery);
 $users = mysql_fetch_assoc($userresult);
 
 
-if ($users) {
+if ($users || is_user_logged_in()) {
 
 	//Flash File Data
 
