d7fcb517584c55ac5264519c7d6f757aa40acc07
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-26 19:31:42 +0000

    Tagging version 1.1.31. Check the change log for more info.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@778929 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.1.31/inc/admin/sql/upgrade_1_1_to_1_3.sql
@@ -0,0 +1,8 @@
+﻿;
+ALTER TABLE ec_shippingrate MODIFY shipping_override_rate float(11,3) NULL DEFAULT NULL;
+ALTER TABLE ec_orderdetail ADD `optionitem_price_1` FLOAT(15,3) NOT NULL DEFAULT '0.000';
+ALTER TABLE ec_orderdetail ADD `optionitem_price_2` FLOAT(15,3) NOT NULL DEFAULT '0.000';
+ALTER TABLE ec_orderdetail ADD `optionitem_price_3` FLOAT(15,3) NOT NULL DEFAULT '0.000';
+ALTER TABLE ec_orderdetail ADD `optionitem_price_4` FLOAT(15,3) NOT NULL DEFAULT '0.000';
+ALTER TABLE ec_orderdetail ADD `optionitem_price_5` FLOAT(15,3) NOT NULL DEFAULT '0.000';
+ALTER TABLE ec_order ADD `order_customer_notes` BLOB;
---
+++ tags/1.1.31/inc/admin/sql/upgrade_1_3_to_1_5.sql
@@ -0,0 +1,37 @@
+﻿;
+ALTER TABLE ec_country ADD `vat_rate_cnt` float(9,3) NOT NULL DEFAULT '0.000';
+CREATE TABLE IF NOT EXISTS `ec_role` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_label` varchar(20) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `admin_access` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`role_id`),
+  UNIQUE KEY `role_id` (`role_id`)
+) ENGINE=MyISAM 
+AUTO_INCREMENT=3 CHARACTER SET 'utf8' COLLATE
+ 'utf8_general_ci'
+;
+CREATE TABLE IF NOT EXISTS `ec_roleprice` (
+  `roleprice_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL DEFAULT '0',
+  `role_label` varchar(20) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `role_price` float(15,3) NOT NULL DEFAULT '0.000',
+  PRIMARY KEY (`roleprice_id`),
+  UNIQUE KEY `roleprice_id` (`roleprice_id`)
+) ENGINE=MyISAM 
+AUTO_INCREMENT=3 CHARACTER SET 'utf8' COLLATE
+ 'utf8_general_ci'
+;
+CREATE TABLE IF NOT EXISTS `ec_roleaccess` (
+  `roleaccess_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_label` varchar(20) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `admin_panel` varchar(20) COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`roleaccess_id`),
+  UNIQUE KEY `roleaccess_id` (`roleaccess_id`)
+) ENGINE=MyISAM 
+AUTO_INCREMENT=100 CHARACTER SET 'utf8' COLLATE
+ 'utf8_general_ci'
+;
+INSERT INTO `ec_role` (`role_id`, `role_label`, `admin_access`) VALUES
+  (1, 'admin', 1),
+  (2, 'shopper', 0)
+;
