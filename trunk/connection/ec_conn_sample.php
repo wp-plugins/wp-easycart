e97041e98891112d2d4c074ebbc6088f506f500f
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-16 01:03:00 +0000

    new conn file sample.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@788474 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/connection/ec_conn_sample.php
+++ trunk/connection/ec_conn_sample.php
@@ -2,5 +2,6 @@
 	define ("HOSTNAME", 'localhost'); 	
 	define ("DATABASE", 'db_name'); 		
 	define ("USERNAME", 'db_user'); 	
-	define ("PASSWORD", 'db_password'); 	
+	define ("PASSWORD", 'db_password'); 
+	define ('WP_PREFIX','wp_');	//change to your db prefix found in wp-config.php file if necessary
 ?>
