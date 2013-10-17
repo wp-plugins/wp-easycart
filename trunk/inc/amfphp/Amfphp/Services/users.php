332b3f747339a1eb8c7f8d23b6de9cda91e2312c
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-17 15:47:19 +0000

    update to stop removing admin users
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@789357 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/inc/amfphp/Amfphp/Services/users.php
+++ trunk/inc/amfphp/Amfphp/Services/users.php
@@ -150,97 +150,119 @@ class users
 			  }
 		}
 		function deleteclient($clientid) {
-			  //Create SQL Query	
-			  $deletesql = $this->escape("DELETE FROM ec_address WHERE ec_address.user_id = '%s'", $clientid);
-			  //Run query on database;
-			  mysql_query($deletesql);
+			  $query= mysql_query("SELECT SQL_CALC_FOUND_ROWS ec_user.* FROM ec_user WHERE ec_user.user_level = 'admin' AND ec_user.user_id != '".$clientid."'");
+			  $totalquery=mysql_query("SELECT FOUND_ROWS()");
+			  $totalrows = mysql_fetch_array($totalquery);
 			  
+			  //return $totalrows;
 			  //Create SQL Query	
-			  $deletesql = $this->escape("DELETE FROM ec_user WHERE ec_user.user_id = '%s'", $clientid);
-			  //Run query on database;
-			  mysql_query($deletesql);
-			  
-			  //if no errors, return their current Client ID
-			  //if results, convert to an array for use in flash
-			  if(!mysql_error()) {
-				  $returnArray[] ="success";
-				  return($returnArray); //return array results if there are some
+			  if ($totalrows[0] >= '1') {
+				  
+				  $deletesql = $this->escape("DELETE FROM ec_address WHERE ec_address.user_id = '%s'", $clientid);
+				  //Run query on database;
+				  mysql_query($deletesql);
+				  
+				  //Create SQL Query	
+				  $deletesql = $this->escape("DELETE FROM ec_user WHERE ec_user.user_id = '%s'", $clientid);
+				  //Run query on database;
+				  mysql_query($deletesql);
+				  
+				  //if no errors, return their current Client ID
+				  //if results, convert to an array for use in flash
+				  if(!mysql_error()) {
+					  $returnArray[] = "success";
+					  return($returnArray); //return array results if there are some
+				  } else {
+					  $returnArray[] = "error";
+					  return $returnArray; //return noresults if there are no results
+				  }
 			  } else {
-				  $returnArray[] = "error";
-				  return $returnArray; //return noresults if there are no results
+				  $returnArray[] = "noadminerror";
+				  return $returnArray; //return noresults if there ares no result
 			  }
 		}
 		function updateclient($clientid, $client) {
+			  $query= mysql_query("SELECT SQL_CALC_FOUND_ROWS ec_user.* FROM ec_user WHERE ec_user.user_level = 'admin'");
+			  $totalquery=mysql_query("SELECT FOUND_ROWS()");
+			  $totalrows = mysql_fetch_array($totalquery);
+			  
 			  //convert object to array
 			  $client = (array)$client;
 			  
-			  
-			  $sql = sprintf("Replace into ec_address(ec_address.address_id, ec_address.user_id, ec_address.first_name, ec_address.last_name, ec_address.address_line_1, ec_address.address_line_2, ec_address.city, ec_address.state, ec_address.zip, ec_address.country, ec_address.phone)
-				values('%s', '".$clientid."', '%s', '%s', '%s',  '%s', '%s', '%s', '%s', '%s', '%s')",
-			  mysql_real_escape_string($client['billing_id']),
-			  mysql_real_escape_string($client['billname']),
-			  mysql_real_escape_string($client['billlastname']),
-			  mysql_real_escape_string($client['billaddress']),
-			  mysql_real_escape_string($client['billaddress2']),
-			  mysql_real_escape_string($client['billcity']),
-			  mysql_real_escape_string($client['billstate']),
-			  mysql_real_escape_string($client['billzip']),
-			  mysql_real_escape_string($client['billcountry']),
-			  mysql_real_escape_string($client['billphone']));
-			  //Run query on database;
-			  mysql_query($sql);
-			  
-			  $default_billing_address_id = $client['billing_id'];
-				
-			  $sql = sprintf("Replace into ec_address(ec_address.address_id, ec_address.user_id, ec_address.first_name, ec_address.last_name, ec_address.address_line_1, ec_address.address_line_2, ec_address.city, ec_address.state, ec_address.zip, ec_address.country, ec_address.phone)
-				values('%s', '".$clientid."', '%s', '%s', '%s',  '%s', '%s', '%s', '%s', '%s', '%s')",	
-			  mysql_real_escape_string($client['shipping_id']),
-			  mysql_real_escape_string($client['shipname']),
-			  mysql_real_escape_string($client['shiplastname']),
-			  mysql_real_escape_string($client['shipaddress']),
-			  mysql_real_escape_string($client['shipaddress2']),
-			  mysql_real_escape_string($client['shipcity']),
-			  mysql_real_escape_string($client['shipstate']),
-			  mysql_real_escape_string($client['shipzip']),
-			  mysql_real_escape_string($client['shipcountry']),
-			  mysql_real_escape_string($client['shipphone']));
-			  //Run query on database;
-			  mysql_query($sql);
-			  $default_shipping_address_id = $client['shipping_id'];
-				
-				
-			  //Create SQL Query
-			  $sql = sprintf("UPDATE ec_user SET ec_user.email = '%s', ec_user.password = '%s', ec_user.first_name = '%s', ec_user.last_name = '%s', ec_user.default_billing_address_id = %d, ec_user.default_shipping_address_id = %d, ec_user.user_level = '%s', ec_user.is_subscriber = %d WHERE ec_user.user_id = %d",
-				mysql_real_escape_string($client['email']),
-				mysql_real_escape_string($client['password']),
-				mysql_real_escape_string($client['firstname']),
-				mysql_real_escape_string($client['lastname']),
-				mysql_real_escape_string($default_billing_address_id),
-				mysql_real_escape_string($default_shipping_address_id),
-				mysql_real_escape_string($client['userlevel']),
-				mysql_real_escape_string($client['subscriber']),
-				mysql_real_escape_string($clientid));
-				
-				//Run query on database;
-			    mysql_query($sql);
-				//return $sql;
-				
-				
-			//Enqueue Quickbooks Update Customer
-			if( file_exists( "../../../../wp-easycart-quickbooks/QuickBooks.php" ) ){
-				$quickbooks = new ec_quickbooks( );
-				$quickbooks->update_user_admin( $clientid );	
-			}				
+			  //return $client['userlevel'];
+			  //Create SQL Query	
+			  if ($totalrows[0] != 1 || $client['userlevel'] == 'admin') {
 
-			//if no errors, return their current Client ID
-			//if results, convert to an array for use in flash
-			if(!mysql_error()) {
-				$returnArray[] ="success";
-				return($returnArray); //return array results if there are some
+				  $sql = sprintf("Replace into ec_address(ec_address.address_id, ec_address.user_id, ec_address.first_name, ec_address.last_name, ec_address.address_line_1, ec_address.address_line_2, ec_address.city, ec_address.state, ec_address.zip, ec_address.country, ec_address.phone)
+					values('%s', '".$clientid."', '%s', '%s', '%s',  '%s', '%s', '%s', '%s', '%s', '%s')",
+				  mysql_real_escape_string($client['billing_id']),
+				  mysql_real_escape_string($client['billname']),
+				  mysql_real_escape_string($client['billlastname']),
+				  mysql_real_escape_string($client['billaddress']),
+				  mysql_real_escape_string($client['billaddress2']),
+				  mysql_real_escape_string($client['billcity']),
+				  mysql_real_escape_string($client['billstate']),
+				  mysql_real_escape_string($client['billzip']),
+				  mysql_real_escape_string($client['billcountry']),
+				  mysql_real_escape_string($client['billphone']));
+				  //Run query on database;
+				  mysql_query($sql);
+				  
+				  $default_billing_address_id = $client['billing_id'];
+					
+				  $sql = sprintf("Replace into ec_address(ec_address.address_id, ec_address.user_id, ec_address.first_name, ec_address.last_name, ec_address.address_line_1, ec_address.address_line_2, ec_address.city, ec_address.state, ec_address.zip, ec_address.country, ec_address.phone)
+					values('%s', '".$clientid."', '%s', '%s', '%s',  '%s', '%s', '%s', '%s', '%s', '%s')",	
+				  mysql_real_escape_string($client['shipping_id']),
+				  mysql_real_escape_string($client['shipname']),
+				  mysql_real_escape_string($client['shiplastname']),
+				  mysql_real_escape_string($client['shipaddress']),
+				  mysql_real_escape_string($client['shipaddress2']),
+				  mysql_real_escape_string($client['shipcity']),
+				  mysql_real_escape_string($client['shipstate']),
+				  mysql_real_escape_string($client['shipzip']),
+				  mysql_real_escape_string($client['shipcountry']),
+				  mysql_real_escape_string($client['shipphone']));
+				  //Run query on database;
+				  mysql_query($sql);
+				  $default_shipping_address_id = $client['shipping_id'];
+					
+					
+				  //Create SQL Query
+				  $sql = sprintf("UPDATE ec_user SET ec_user.email = '%s', ec_user.password = '%s', ec_user.first_name = '%s', ec_user.last_name = '%s', ec_user.default_billing_address_id = %d, ec_user.default_shipping_address_id = %d, ec_user.user_level = '%s', ec_user.is_subscriber = %d WHERE ec_user.user_id = %d",
+					mysql_real_escape_string($client['email']),
+					mysql_real_escape_string($client['password']),
+					mysql_real_escape_string($client['firstname']),
+					mysql_real_escape_string($client['lastname']),
+					mysql_real_escape_string($default_billing_address_id),
+					mysql_real_escape_string($default_shipping_address_id),
+					mysql_real_escape_string($client['userlevel']),
+					mysql_real_escape_string($client['subscriber']),
+					mysql_real_escape_string($clientid));
+					
+					//Run query on database;
+					mysql_query($sql);
+					//return $sql;
+					
+					
+				//Enqueue Quickbooks Update Customer
+				if( file_exists( "../../../../wp-easycart-quickbooks/QuickBooks.php" ) ){
+					$quickbooks = new ec_quickbooks( );
+					$quickbooks->update_user_admin( $clientid );	
+				}				
+	
+				//if no errors, return their current Client ID
+				//if results, convert to an array for use in flash
+				if(!mysql_error()) {
+					$returnArray[] ="success";
+					return($returnArray); //return array results if there are some
+				} else {
+					$returnArray[] = "error";
+					return $returnArray; //return noresults if there are no results
+				}
 			} else {
-				$returnArray[] = "error";
-				return $returnArray; //return noresults if there are no results
-			}
+				  $returnArray[] = "noadminerror";
+				  return $returnArray; //return noresults if there ares no result
+			  }
 		}
 		
 		
