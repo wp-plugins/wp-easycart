28396b3dc69a4b01239146feeb64472a5bb1379a
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-06 21:20:20 +0000

    Tagging version 1.2.11, check the change log for more info.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@800060 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.2.11/design/layout/default-nonresponsive-v1/ec_product.php
@@ -0,0 +1,114 @@
+<div class="ec_product_quick_view_box_holder" id="ec_product_quick_view_box_<?php echo $product->model_number; ?>">
+	<div class="ec_product_quick_view_box_background"></div>
+    <div class="ec_product_quick_view_box_content_holder">
+    	<div class="ec_product_quick_view_box_content_close">
+        	<a href="#" onclick="ec_product_hide_quick_view( '<?php echo $product->model_number; ?>' )">X</a>
+        </div>
+        <?php $product->display_product_details_form_start( ); ?>
+		<?php if( $product->has_thumbnails( ) ){ ?>
+    	<div class="ec_product_quick_view_box_content_thumbnails">
+        	<?php $product->display_product_image_thumbnails( "xsmall", "ec_thumb_quick_view_", "ec_thumb_quick_view_click" ); ?>
+        </div>
+        <?php } ?>
+        <div class="ec_product_quick_view_box_content_image">
+        	<?php $product->display_product_details_image_set( "large", "ec_image_quick_view_", "ec_image_quick_view_click" ); ?>
+        </div>
+        <div class="ec_product_quick_view_box_content_right">
+        	<div class="ec_product_quick_view_box_content_row"><span class="title"><?php $product->display_product_title(); ?></span></div>
+        	<?php if( $product->is_donation ){?>
+                <div class="ec_product_quick_view_box_content_row" id="ec_product_details_donation_row"> <?php echo $GLOBALS['language']->get_text( 'product_details', 'product_details_donation_label' )?> <?php $product->display_price_input(); ?></div>
+                
+                <div class="ec_product_details_donation" id="ec_product_details_donation_min_row">( <?php echo $GLOBALS['language']->get_text( 'product_details', 'product_details_minimum_donation' )?> <?php $product->display_price( ); ?> )</div>
+        
+            <?php }else{ ?>
+                <div class="ec_product_quick_view_box_content_row2"><?php $product->display_price(); ?>&nbsp;<?php $product->display_product_list_price(); ?></div>
+                <?php if( $product->list_price != "0.00" ){ ?>
+                <div class="ec_product_quick_view_box_content_row3"><span class="ec_product_details_discount"><?php echo $GLOBALS['language']->get_text( 'product_details', 'product_details_reduced_price' )?> <?php $product->display_product_discount_percentage( ); ?>%</span></div>
+                <?php } ?> 
+                <div class="ec_product_quick_view_box_content_row"><?php $product->display_product_price_tiers(); ?></div>
+            <?php }?>
+            
+            <?php if( $product->product_has_swatches( $product->options->optionset1 ) ){ ?>
+            <div class="ec_product_quick_view_box_content_option_row">
+              <?php $product->display_product_option( $product->options->optionset1, "large", 1, "ec_swatch_quick_view_", "ec_swatch_click" ); ?>
+            </div>
+            <?php }else if( $product->product_has_combo( $product->options->optionset1 ) ){ ?>
+            <div class="ec_product_quick_view_box_content_option_row">
+              <?php $product->display_product_option( $product->options->optionset1, "large", 1, "ec_combo_", "" ); ?>
+            </div>
+            <?php }?>
+            <?php if( $product->product_has_swatches( $product->options->optionset2 ) ){ ?>
+            <div class="ec_product_quick_view_box_content_option_row">
+              <?php $product->display_product_option( $product->options->optionset2, "large", 2, "ec_swatch_quick_view_", "ec_swatch_click" ); ?>
+            </div>
+            <?php }else if( $product->product_has_combo( $product->options->optionset2 ) ){ ?>
+            <div class="ec_product_quick_view_box_content_option_row">
+              <?php $product->display_product_option( $product->options->optionset2, "large", 2, "ec_combo_", "" ); ?>
+            </div>
+            <?php }?>
+            <?php if( $product->product_has_swatches( $product->options->optionset3 ) ){ ?>
+            <div class="ec_product_quick_view_box_content_option_row">
+              <?php $product->display_product_option( $product->options->optionset3, "large", 3, "ec_swatch_quick_view_", "ec_swatch_click" ); ?>
+            </div>
+            <?php }else if( $product->product_has_combo( $product->options->optionset3 ) ){ ?>
+            <div class="ec_product_quick_view_box_content_option_row">
+              <?php $product->display_product_option( $product->options->optionset3, "large", 3, "ec_combo_", "" ); ?>
+            </div>
+            <?php }?>
+            <?php if( $product->product_has_swatches( $product->options->optionset4 ) ){ ?>
+            <div class="ec_product_quick_view_box_content_option_row">
+              <?php $product->display_product_option( $product->options->optionset4, "large", 4, "ec_swatch_quick_view_", "ec_swatch_click" ); ?>
+            </div>
+            <?php }else if( $product->product_has_combo( $product->options->optionset4 ) ){ ?>
+            <div class="ec_product_quick_view_box_content_option_row">
+              <?php $product->display_product_option( $product->options->optionset4, "large", 4, "ec_combo_", "" ); ?>
+            </div>
+            <?php }?>
+            <?php if( $product->product_has_swatches( $product->options->optionset5 ) ){ ?>
+            <div class="ec_product_quick_view_box_content_option_row">
+              <?php $product->display_product_option( $product->options->optionset5, "large", 5, "ec_swatch_quick_view_", "ec_swatch_click" ); ?>
+            </div>
+            <?php }else if( $product->product_has_combo( $product->options->optionset5 ) ){ ?>
+            <div class="ec_product_quick_view_box_content_option_row">
+              <?php $product->display_product_option( $product->options->optionset5, "large", 5, "ec_combo_", "" ); ?>
+            </div>
+            <?php }?>
+            <?php if( $product->is_giftcard ){ ?>
+            <div class="ec_product_details_gift_card">
+              <?php $product->display_gift_card_input(); ?>
+            </div>
+            <?php }?>
+            
+            <div class="ec_product_details_quantity" id="ec_product_details_quantity_<?php echo $product->model_number; ?>"><?php echo $GLOBALS['language']->get_text( 'product_details', 'product_details_quantity' )?> <?php $product->display_product_quantity_input("1"); ?></div>
+            
+            <?php if( $product->show_stock_quantity || $product->use_optionitem_quantity_tracking ){ ?>
+            <div class="ec_product_quick_view_box_content_row"><span id="in_stock_amount_text_<?php echo $product->model_number; ?>"><?php $product->display_product_stock_quantity(); ?></span> <?php echo $GLOBALS['language']->get_text( 'product_details', 'product_details_remaining' )?></div>
+            <?php }?>
+            <div class="ec_product_quick_view_box_content_row"><?php $product->display_product_add_to_cart_button( $GLOBALS['language']->get_text( 'quick_view', 'quick_view_add_to_cart' ), "ec_quick_view_error" ); ?></div>
+            <div class="ec_product_quick_view_box_content_row"><?php $product->display_product_link( $GLOBALS['language']->get_text( 'quick_view', 'quick_view_view_full_details' ) ); ?></div>
+        </div>
+        <?php $product->display_product_details_form_end( ); ?>
+    </div>
+</div>
+<script>
+jQuery("#ec_product_quick_view_box_<?php echo $product->model_number; ?>").appendTo("body");
+</script>
+<div id="ec_product_item<?php echo $i+1; ?>" class="ec_product">
+    <div class="ec_product_quick_view_holder"><?php $product->display_product_quick_view( $GLOBALS['language']->get_text( 'product_page', 'product_quick_view' ) ); ?></div>
+    
+    <div class="ec_product_images">
+		<?php $product->display_product_image_set( "medium", "ec_image_", "" ); ?>
+		
+    </div>
+	
+    <div class="ec_product_left">
+        <div class="ec_product_title"><?php $product->display_product_title_link(); ?></div>
+        
+        <?php if( $product->use_customer_reviews ){?><div class="ec_product_rating"><div class="ec_product_stars"><?php $product->display_product_stars(); ?></div><div class="ec_product_num_reviews"> (<?php $product->display_product_number_reviews(); ?>)</div></div><?php }?>
+    	
+    </div>
+    <div style="width:35%; float:right;">
+    	<div class="ec_product_pricing"><?php $product->display_product_list_price(); ?><?php $product->display_product_price(); ?></div>
+    </div>
+    <?php if($product->use_optionitem_images){ ?><div class="ec_product_swatches"><?php $product->display_product_option_swatches( $product->options->optionset1, "small", 1, "ec_swatch_", "ec_swatch_click", false ); ?></div><?php }?>
+</div>
---
+++ tags/1.2.11/inc/admin/admin_console.php
@@ -0,0 +1,234 @@
+<?php 
+
+$validate = new ec_validation; 
+$license = new ec_license;
+
+if( isset( $_GET['dismiss_lite_banner'] ) ){
+	update_option( 'ec_option_show_lite_message', '0' );	
+}
+
+//get admin request ID
+$db = new ec_db_admin( );
+$users = $db->get_users( );
+$admin_password_hash = "";
+foreach( $users as $user ){
+	if( $user->user_level == "admin" ){
+		$admin_password_hash = $user->password;
+	}
+}
+$reqid = $admin_password_hash;
+
+
+
+//get the site url without http:// https:// or www.
+$input = site_url();
+if (!preg_match('#^http(s)?://#', $input)) {
+    $input = 'http://' . $input;
+}
+$urlParts = parse_url($input);
+
+if(isset($urlParts['path'])) {
+	$domain = $urlParts['host'] . $urlParts['path'];
+} else {
+	$domain = $urlParts['host'];
+}
+
+
+//get current wordpress user
+global $current_user;
+get_currentuserinfo();
+$userlogin = $current_user->user_login;
+$useremail = $current_user->user_email;
+
+//check if using https://
+$is_secure = 'false';
+if (is_ssl()) {
+	$is_secure = 'true';
+}
+
+?>
+
+<div class="wrap">
+<?php if( !$license->is_registered() && !$license->is_lite_version() ) { ?>
+<div class="ribbon">You are running the WP EasyCart FREE version. To purchase the LITE or FULL version and unlock the full selling potential visit <a href="http://www.wpeasycart.com?ecid=admin_console" target="_blank">www.wpeasycart.com</a> and purchase a license.</div>
+<?php }else if( $license->is_lite_version() && $license->show_lite_message() ) { ?>
+<div class="ribbon">You are running the WP EasyCart Lite version. To learn more about what you are missing with the Full version visit <a href="http://www.wpeasycart.com?ecid=admin_console" target="_blank">www.wpeasycart.com</a>. To dismiss this banner <a href="?page=ec_adminconsole&dismiss_lite_banner=true">click here.</a></div>
+<?php }?>
+<h2>
+  
+  <img src="<?php echo plugins_url('images/WP-Easy-Cart-Logo.png', __FILE__); ?>" /></h2>
+<div class="ec_contentwrap">
+   
+    <h2>Administrative Console</h2>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td height="55" colspan="2"><p>The WordPress EasyCart administrative console software is available for Desktops and popular tablet and phone systems.  The administrative console software allows you to manage day to day operations of your store, including Orders, Products, Users, Marketing, and more!</p>
+        <p><em><strong>Note:</strong> The administrative console is not downloaded or associated with WordPress and/or it's site. This software is downloaded via 3rd party systems such as Apple iTunes, the Google Play store, and WP Easy Cart.</em></p></td>
+      </tr>
+      <tr>
+        <td height="25" class="platformheading">WordPress Embedded Adminstration Console</td>
+        <td height="25" class="platformheadingimage"><img src="<?php echo plugins_url('images/wordpress_icon.jpg', __FILE__); ?>" width="38" height="40" /></td>
+      </tr>
+      <tr>
+        <td height="10" colspan="2"></td>
+      </tr>
+      <tr id="ec_wordpress_content">
+      	<?php if( function_exists( "wp_easycart_load_admin" ) ){ ?>
+        <td width="100%" colspan="2" align="center">
+        	 <?php wp_easycart_load_admin( $domain, $userlogin, $reqid, $is_secure ); ?>
+        </td>
+        <?php }else{ ?>
+        <td width="600" align="center">
+        	<a href="http://wpeasycart.com/air/wp-easycart-admin.zip" target="_blank"><img src="<?php echo plugins_url('images/wordpress_easycart.jpg', __FILE__); ?>" alt="iPad Administration Console" width="600" height="360" /></a>
+        </td>
+        <td  style="padding: 15px;">
+        	&nbsp;&nbsp;&nbsp;&nbsp;
+            <p><strong>How to Install: </strong></p>
+            <p><strong>1. </strong><a href="http://wpeasycart.com/air/wp-easycart-admin.zip">Click this link</a> to download the WP EasyCart Administration Plugin.<br />
+            Note: MAC users may have an option set in Safari that automatically opens the zip. You should disable this option: Uncheck &quot;Open Safe files after downloading&quot; in Safari preferences.</p>
+            <p><strong>2. </strong>Click 'Plugins' -> 'Add New' -> 'Upload' -> 'Choose File' and select the plugin from your downloads. Once selected, click install now.</p>
+            <p><strong>3. </strong>Once Installed, this section will be replaced with an embedded version of the WP EasyCart Administration Software.</p>
+            <p>&nbsp;</p>
+            <p><strong>Need More Help?</strong></p>
+            <p><a href="http://www.wpeasycart.com/support-ticket/" target="_blank">Submit a ticket</a> with the url you are using and an agent will get back to you shortly.</p>
+        </td>
+        <?php }?>
+      </tr>
+      
+      <tr>
+        <td width="600">&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      
+      <tr>
+        <td height="25" class="platformheading">Desktop and Laptop Users</td>
+        <td height="25" class="platformheadingimage"><img src="<?php echo plugins_url('images/windows-apple-linux.png', __FILE__); ?>" width="120" height="40" /></td>
+      </tr>
+      <tr>
+        <td height="10" colspan="2"></td>
+      </tr>
+      <tr id="ec_desktop_content">
+        <td width="600" align="center">
+        
+        <script type="text/javascript" src="<?php echo plugins_url('AIR_badge/swfobject.js', __FILE__); ?>"></script>
+        
+        <!-- BEGIN EMBED CODE -->
+            
+            <!-- IMPORTANT: Make sure you also copy the swfobject script tag from the head above -->
+            
+            <div id="flashcontent" style="width:600px; height:360px;">
+                <strong>Please upgrade your Flash Player</strong>
+                This is the content that would be shown if the user does not have Flash Player 10.0.0 or higher installed.
+            </div>
+        
+          <script type="text/javascript">
+                // <![CDATA[
+                
+                // version 9.0.115 or greater is required for launching AIR apps.
+                var so = new SWFObject("<?php echo plugins_url('AIR_badge/AIRInstallBadge.swf', __FILE__); ?>", "Badge", "600", "360", "10.0.0", "#FFFFFF");
+                so.useExpressInstall('<?php echo plugins_url('AIR_badge/expressinstall.swf', __FILE__); ?>');
+                
+                // these parameters are required for badge install:
+                so.addVariable("airversion", "3.7"); // version of AIR runtime required
+                so.addVariable("appname", "WP EasyCart Admin Console"); // application name to display to the user
+                so.addVariable("appurl", "http://www.wpeasycart.com/air/wpadmin.air"); // absolute URL (beginning with http or https) of the application ".air" file
+                
+                // these parameters are required to support launching apps from the badge (but optional for install):
+                so.addVariable("appid", "com.wpeasycart.admin"); // the qualified application ID (ex. com.gskinner.air.MyApplication)
+                so.addVariable("pubid", ""); // publisher id
+                
+                // this parameter is required in addition to the above to support upgrading from the badge:
+                so.addVariable("appversion", "1.1.0"); // AIR application version
+                
+                // these parameters are optional:
+                so.addVariable("image", "<?php echo plugins_url('AIR_badge/DemoImage.jpg', __FILE__); ?>"); // URL for an image (JPG, PNG, GIF) or SWF to display in the badge (205px wide, 170px high)
+                so.addVariable("appinstallarg", "installed from web"); // passed to the application when it is installed from the badge
+                so.addVariable("applauncharg", "launched from web"); // passed to the application when it is launched from the badge
+                so.addVariable("helpurl", "http://www.wpeasycart.com/docs"); // optional url to a page containing additional help, displayed in the badge's help screen
+                so.addVariable("hidehelp", "false"); // hides the help icon if "true"
+                so.addVariable("skiptransition", "true"); // skips the initial transition if "true"
+                so.addVariable("titlecolor", "#4b9725"); // changes the color of titles
+                so.addVariable("buttonlabelcolor", "#4b9725"); // changes the color of the button label
+                so.addVariable("appnamecolor", "#4b9725"); // changes the color of the application name if the image is not specified or loaded
+                
+                // these parameters allow you to override the default text in the badge:
+                // supported strings: str_error, str_err_params, str_err_airunavailable, str_err_airswf, str_loading, str_install, str_launch, str_upgrade, str_close, str_launching, str_launchingtext, str_installing, str_installingtext, str_tryagain, str_beta3, str_beta3text, str_help, str_helptext
+                so.addVariable("str_err_airswf", "<u>Running locally?</u><br/><br/>The AIR proxy swf won't load properly when this demo is run from the local file system."); // overrides the error text when the AIR proxy swf fails to load
+                
+                so.write("flashcontent");
+                
+                // ]]>
+            </script>
+        
+        <!-- END EMBED CODE -->
+        
+        
+        
+        </td>
+        <td style="padding: 15px;">&nbsp;&nbsp;&nbsp;&nbsp;
+        <p><strong>How to Install: </strong></p>
+        <p><strong>1. </strong>Simply click the '<em><strong>Install Now</strong></em>' on the left image and the automatic installer will try to install Adobe AIR and the Administrative Console. Please allow a few moments for it to install.</p>
+        <p>Note: If that process fails or the automatic installer does not work, you can try our second method.</p>
+        <p><strong>2. </strong> Try manually installing by following these <a href="http://www.wpeasycart.com/air-install" target="_blank">2 steps to install.</a></p>
+        <p>&nbsp;</p>
+        <p><strong>Need More Help?</strong></p>
+        <p>Try visiting our <a href="http://www.wpeasycart.com/video-tutorials/" target="_blank">Video Tutorial</a> on installing and logging into the admin console, or our <a href="http://www.wpeasycart.com/docs/1.0.0/administration/installing_console.php" target="_blank">Online Documentation</a>.</p>
+        <p>Still need more help? Simply <a href="http://www.wpeasycart.com/support-ticket/" target="_blank">submit a ticket</a> with the url you are using and an agent will get back to you shortly.</p></td>
+      </tr>
+      
+      <tr>
+        <td width="600">&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td height="25" class="platformheading">iPad Users</td>
+        <td height="25" class="platformheadingimage"><img src="<?php echo plugins_url('images/apple.png', __FILE__); ?>" width="40" height="40" /></td>
+      </tr>
+      <tr>
+        <td height="10" colspan="2"></td>
+      </tr>
+      <tr id="ec_ipad_content">
+        <td width="600" align="center"><a href="https://itunes.apple.com/us/app/wp-easycart/id616846878?mt=8" target="_blank"><img src="<?php echo plugins_url('images/ipad_mockup1.png', __FILE__); ?>" alt="iPad Administration Console" width="550" height="432" /></a></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <p><strong>How to Install: </strong></p>
+        <p><strong>1. </strong>Simply click the iPad to visit iTunes and download the app from the Apple iTunes store.</p>
+        <p><strong><br />
+          Need More Help?</strong></p>
+        <p>Try visiting our <a href="http://www.wpeasycart.com/video-tutorials/" target="_blank">Video Tutorial</a> on installing and logging into the admin console, or our <a href="http://www.wpeasycart.com/docs/1.0.0/administration/installing_console.php" target="_blank">Online Documentation</a>.</p>
+        <p>Still need more help? Simply <a href="http://www.wpeasycart.com/support-ticket/" target="_blank">submit a ticket</a> with the url you are using and an agent will get back to you shortly.</p></td>
+      </tr>
+      <tr>
+        <td width="600">&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td height="25" class="platformheading">Android Tablet and Phone Users</td>
+        <td height="25" class="platformheadingimage"><img src="<?php echo plugins_url('images/android_vector.png', __FILE__); ?>" width="38" height="40" /></td>
+      </tr>
+      <tr>
+        <td colspan="2" height="10"></td>
+      </tr>
+      <tr id="ec_android_content">
+        <td width="600" align="center"><a href="https://play.google.com/store/search?q=wp+easycart&amp;c=apps&amp;feature=spelling" target="_blank"><img src="<?php echo plugins_url('images/android_tablet_phone.png', __FILE__); ?>" width="550" height="450" /></a></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <p><strong>How to Install: </strong></p>
+        <p><strong>1. </strong>Simply click the tablet to visit the Google Play network and download the app from the Play app store.</p>
+        <p><br />
+          <strong>Need More Help?</strong></p>
+        <p>Try visiting our <a href="http://www.wpeasycart.com/video-tutorials/" target="_blank">Video Tutorial</a> on installing and logging into the admin console, or our <a href="http://www.wpeasycart.com/docs/1.0.0/administration/installing_console.php" target="_blank">Online Documentation</a>.</p>
+        <p>Still need more help? Simply <a href="http://www.wpeasycart.com/support-ticket/" target="_blank">submit a ticket</a> with the url you are using and an agent will get back to you shortly.</p>
+        <p>&nbsp; </p></td>
+      </tr>
+      <tr>
+        <td width="600">&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+</div>
+</div>
+<script type="text/javascript">
+function ec_admin_open( panel ){
+	jQuery( '#' + panel + "_content" ).show('blind');
+}
+</script>
