27a8035d4da84180ffb09887bef6a62d175755c6
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-14 03:59:21 +0000

    One more fix for updating.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart@804151 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.2.14/inc/scripts/ec_css_loader.php
+++ tags/1.2.14/inc/scripts/ec_css_loader.php
@@ -94,7 +94,7 @@ foreach ($ec_account_css as $css_file ){
 foreach ($ec_widget_css as $css_file ){
 	if( file_exists( WP_PLUGIN_DIR . '/wp-easycart-data/design/theme/' . get_option( 'ec_option_base_theme' ) . '/ec_' . $css_file . '/ec_' . $css_file . '.css' ) )
 		$css_content .= file_get_contents( WP_PLUGIN_DIR . '/wp-easycart-data/design/theme/' . get_option( 'ec_option_base_theme' ) . '/ec_' . $css_file . '/ec_' . $css_file . '.css' );
-	else
+	else if( file_exists( WP_PLUGIN_DIR . '/wp-easycart/design/theme/' . get_option( 'ec_option_base_theme' ) . '/ec_' . $css_file . '/ec_' . $css_file . '.css' ) )
 		$css_content .= file_get_contents( WP_PLUGIN_DIR . "/" . EC_PLUGIN_DIRECTORY . '/design/theme/' . get_option( 'ec_option_base_theme' ) . '/ec_' . $css_file . '/ec_' . $css_file . '.css' );
 }
 
