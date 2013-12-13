c2c4680b9129da47b47c1e6f27e7f2dc320e869d
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-13 20:11:13 +0000

    Adding the shell file for our new admin. More to come.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart/trunk@821387 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/admin/admin_v2.php
@@ -0,0 +1,59 @@
+<?php require_once( "assets/inc/adminv2_functions.php" ); ?>
+<?php include( "assets/menus/mainmenu.php" ); ?>
+<div class="ec_admin_holder<?php if( isset( $_GET['ec_panel'] ) && ($_GET['ec_panel'] == "admin" || $_GET['ec_panel'] == "demos" || $_GET['ec_panel'] == "support") ){ echo "_console"; } ?>">
+	<?php
+	if( isset( $_GET['ec_page'] ) && $_GET['ec_page'] == "dashboard" ){
+		include( "assets/menus/leftmenu_dashboard.php" );
+	}else if( isset( $_GET['ec_page'] ) && $_GET['ec_page'] == "store-setup" ){
+		include( "assets/menus/leftmenu_setup.php" );
+	}else if( isset( $_GET['ec_page'] ) && $_GET['ec_page'] == "admin-console" ){
+		// Hide this section
+	}else if( isset( $_GET['ec_page'] ) && $_GET['ec_page'] == "online-demos" ){
+		//include( "assets/menus/leftmenu_online_demos.php" );
+	}else if( isset( $_GET['ec_page'] ) && $_GET['ec_page'] == "help-support" ){
+		//include( "assets/menus/leftmenu_help.php" );
+	}else{
+		include( "assets/menus/leftmenu_dashboard.php" );
+	}
+	?>
+    
+    <?php 
+	if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "admin" ){
+		include( "assets/panels/admin_console.php" );
+	} else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "demos" ){
+		include( "assets/panels/demos.php" );
+	} else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "support" ){
+		include( "assets/panels/support.php" );
+	} else { ?>
+    <div class="ec_admin_content">
+    	<div class="ec_admin_content_inner">
+        	<?php 
+			include( "assets/elements/popup.php" );
+			if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "statistics" ){
+				include( "assets/panels/dashboard_statistics.php" );
+			}else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "store-status" ){
+				include( "assets/panels/dashboard_status.php" );
+			}else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "backup-store" ){
+				include( "assets/panels/dashboard_backup.php" );
+			}else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "basic-setup" ){
+				include( "assets/panels/basic_setup.php" );
+			}else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "basic-settings" ){
+				include( "assets/panels/basic_settings.php" );
+			}else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "colorize-easycart" ){
+				include( "assets/panels/colorize_easycart.php" );
+			}else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "payment-settings" ){
+				include( "assets/panels/payment_settings.php" );
+			}else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "advanced-design" ){
+				include( WP_PLUGIN_DIR . "/" . EC_PLUGIN_DIRECTORY . "/design/theme/" . get_option('ec_option_base_theme') . "/admin_panel.php");
+			}else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "advanced-language" ){
+				include( "assets/panels/advanced_language.php" );
+			}else if( isset( $_GET['ec_panel'] ) && $_GET['ec_panel'] == "advanced-setup" ){
+				include( "assets/panels/advanced_setup.php" );
+			}else{
+				include( "assets/panels/dashboard_statistics.php" );
+			}
+			?>
+        </div>
+    </div>
+    <?php } ?>
+</div>
