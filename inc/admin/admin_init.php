e4849a11214277130c3dd80e72297bf457c5355c
levelfourstorefront <levelfourstorefront@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-26 20:23:14 +0000

    Committing fix for admin version number check.
    git-svn-id: https://plugins.svn.wordpress.org/wp-easycart/tags/3.0.21@1100680 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/admin/admin_init.php
+++ inc/admin/admin_init.php
@@ -49,10 +49,10 @@ function ec_install_admin_notice() {
 		<?php	
 		}
 		
-		if( is_plugin_active( "wp-easycart-admin/wpeasycart-admin.php" ) && EC_AD_CURRENT_VERSION != "3.0.12" ){
+		if( is_plugin_active( "wp-easycart-admin/wpeasycart-admin.php" ) && EC_AD_CURRENT_VERSION != "3.0.13" ){
 			?>
 			<div class="error">
-				<p>The latest WP EasyCart Store Admin version is 3.0.12, please update for best results!</p>
+				<p>The latest WP EasyCart Store Admin version is 3.0.13, please update for best results!</p>
 			</div>
 			<?php
 		}
